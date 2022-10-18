99d3ced9af6ec19ffbaf3daa019395beb8690757
Archie Hanafi <101478973+meneerhanafi@users.noreply.github.com>
2022-10-17 20:29:54 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const logo = document.querySelector('.logo');
+
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('is-active');
+    document.body.classList.toggle('nav-open');
+});

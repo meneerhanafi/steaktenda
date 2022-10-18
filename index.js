79444e9b0567fde844b90b07b5c6223cdd69f412
Archie Hanafi <101478973+meneerhanafi@users.noreply.github.com>
2022-10-17 21:48:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.body.addEventListener("touchstart", playVideo);
-    function playVideo() {
-        const video = document.getElementById('myVideo');
-        if(video.playing) {
-        } else {
-            video.play();
-        }
-    }
-});
+
 
 
 

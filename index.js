f261808e604aaf08216fc6b0df2605acdf24889d
Archie Hanafi <101478973+meneerhanafi@users.noreply.github.com>
2022-10-17 21:38:53 -0400

    test

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.body.addEventListener("touchstart", playVideo);
+    function playVideo() {
+        const video = document.getElementById('myVideo');
+        if(video.playing) {
+        } else {
+            video.play();
+        }
+    }
+});
+
+
+
+
 const hamburger = document.querySelector('.hamburger');
 const logo = document.querySelector('.logo');
 
